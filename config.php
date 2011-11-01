41464407dcbd2782d97b2a6bfcedf362f3573991
Júlio Bueno <julioce@gmail.com>
2011-10-31 22:38:03 -0200

    Versão cleanup 1

--- config.php
+++
@@ -1,8 +0,0 @@
-<?
-/* config.php */
-/* Consumer key from twitter */
-$consumer_key = 'eSWSQWbtOxtj5DJYz9I7dQ'; 
-
-/* Consumer Secret from twitter */
-$consumer_secret = 'B3SPTXqFrgW4c4SsqrQlyyXuvQWTzdxyD6S8nI';
-?>
